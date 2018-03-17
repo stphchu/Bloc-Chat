9aff86b16bb42f714155a99cdc09edc1e8adb2e2
Stephanie <esceeaitchyu@gmail.com>
2018-03-16 17:08:06 -0700

    Checkpoint 2 - List chat rooms

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,9 @@
 (function() {
-    function HomeCtrl() {
+    function HomeCtrl($scope, Room) {
+      $scope.roomList = Room.all;
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', [HomeCtrl]);
+        .controller('HomeCtrl', ['$scope', 'Room', HomeCtrl]);
 })();
